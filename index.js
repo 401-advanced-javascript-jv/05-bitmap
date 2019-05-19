5d6d47f2eb1c78f5606f9d4c361f63e1cd620b7d
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-05-18 17:04:49 -0700

    pulls out color palette info to rgb triples

--- index.js
+++ index.js
@@ -2,13 +2,17 @@
 
 const fs = require('fs');
 
+// These are the byte offsets where the named information is stored in the BMP file
+const bitmap16Offsets = {
+  bitDepth: 28,
+};
+
 const bitmap32Offsets = {
-  bitmapSize: 21,
+  bitmapSize: 2,
   pixelArray: 10,
   headerSize: 14,
   width: 18,
   height: 22,
-  bitDepth: 28,
   compressionMethod: 30,
   paletteColorCount: 46,
 };
@@ -32,41 +36,71 @@ class Bitmap {
     this.buffer = buffer;
     this.type = buffer.toString('utf-8', 0, 2);
 
-    if (this.type !== 'BM') { this.invalid = true; }
-
-    for (let offset in bitmap32Offsets) {
-      this.info[offset] = buffer.readInt32LE(bitmap32Offsets[offset]);
+    // file must be type 'BM', or it's not a valid bitmap
+    if (this.type !== 'BM') {
+      this.invalid = true;
+      return;
     }
 
-    console.log(this);
+    for (let offset16 in bitmap16Offsets) {
+      this.info[offset16] = buffer.readInt16LE(bitmap16Offsets[offset16]);
+    }
 
+    for (let offset32 in bitmap32Offsets) {
+      this.info[offset32] = buffer.readInt32LE(bitmap32Offsets[offset32]);
+    }
+
+    // compressionMethod isn't something we can handle
+    if ( this.info.compressionMethod) {
+      this.invalid = true;
+      return;
+    }
+
+    // stride is the number of bytes per row
+    // rows are padded up to a multiple of 4 bytes
+    // bitDepth * width = # bits per row
+    // There are 32 bits per 4-byte block
+    // dividing by this will give the number of 4 byte blocks per row
+    // Math.ceil to round up. Multiply by 4 to get the number of bytes per row
+    this.info.stride = Math.ceil(this.info.bitDepth * this.info.width / 32) * 4;
+
+    this.info.colorPaletteOffset = 14 + this.info.headerSize;
+
+    // If there are colors in the indexed palette, they are 4-byte blocks
+    // Stored as B G R 0
+    if (this.info.paletteColorCount) {
+      this.info.colorPalette = new Array(this.info.paletteColorCount);
+      for(let i = 0; i < this.info.paletteColorCount; i++) {
+        // each color is a multiple of 4 bytes, hence i*4 below
+        this.info.colorPalette[i] = {
+          r: this.buffer.readUInt8(this.info.colorPaletteOffset + i*4 + 2),
+          g: this.buffer.readUInt8(this.info.colorPaletteOffset + i*4 + 1),
+          b: this.buffer.readUInt8(this.info.colorPaletteOffset + i*4 + 0),
+        };
+      }
+    }
+    console.log(this.info);
   }
   /**
    * Transform a bitmap using some set of rules. The operation points to some function, which will operate on a bitmap instance
    * @param operation
    */
   transform(operation) {
-    transforms[operation](this);
     switch (operation) {
+    case 'colors':
+    case 'colours':
+      transformColors(this);
+      break;
     case 'invert':
       doTheInversion(this);
       break;
-    case 'greyscale':
-    case 'grayscale':
-      transformGreyscale(this);
-      break;
-    case 'skew':
-      skewByOne(this);
-      break;
-    default:
-      console.log('An invalid operation was supplied');
-      return this;
+    case 'lines':
+      lines(this);
     }
+
     this.newFile = this.file.replace(/\.bmp$/, `.${operation}.bmp`);
   }
 }
-
-// These are the byte offsets where the named information is stored in the BMP file
 
 /**
  * Sample Transformer (greyscale)
@@ -74,29 +108,23 @@ class Bitmap {
  * Pro Tip: Use "pass by reference" to alter the bitmap's buffer in place so you don't have to pass it around ...
  * @param bmp
  */
-const transformGreyscale = (bmp) => {
+const transformColors = (bmp) => {
   console.log('Transforming bitmap into greyscale', bmp);
-
   //TODO: Figure out a way to validate that the bmp instance is actually valid before trying to transform it
 
   //TODO: alter bmp to make the image greyscale ...
 };
 
 const doTheInversion = (bmp) => {
-  bmp = {};
+  // TODO: Build an inverted pixelArray: Read row-by-row and concat to front of destination buffer, row-by-row
+  // buffer.concat([readrow, buffer]);
+  console.log('inverting image');
 };
 
-const skewByOne = (bmp) => {
-  bmp = {};
-};
-
-/**
- * A dictionary of transformations
- * Each property represents a transformation that someone could enter on the command line and then a function that would be called on the bitmap to do this job
- */
-const transforms = {
-  greyscale: transformGreyscale,
-  invert: doTheInversion,
+const lines = (bmp) => {
+  // change every-other-row to black, starting at row 0
+  // 
+  console.log('adding lines');
 };
 
 // ------------------ GET TO WORK ------------------- //
@@ -110,7 +138,13 @@ function transformWithCallbacks() {
 
     bitmap.parse(buffer);
 
-    if (bitmap.invalid) { throw 'File is not a valid bitmap'; }
+    if (bitmap.invalid) {
+      throw 'File is not a valid bitmap';
+    }
+
+    if (bitmap.invalidTransfor) {
+      throw 'Invalid transformation requested';
+    }
 
     // bitmap.transform(operation);
 
